1d8c4628624fca19ab622bf28d1e5ac3b1a1ed99
yangxiangyang <1161108412@qq.com>
2016-01-09 11:32:40 +0800

    封装2

---
+++ src/main/java/com/mobile/util/AppUtil.java
@@ -0,0 +1,18 @@
+package com.mobile.util;
+
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.util.NodeList;
+
+public class AppUtil {
+	
+//	public static String getAppName(){
+//		//获取应用名称
+//		TagNameFilter tagNameFilter=new TagNameFilter("title");
+//		NodeList nodeList = parser.parse(tagNameFilter);
+//		String titleString = nodeList.elementAt(0).toPlainTextString();
+//		String appname = titleString.substring(0,titleString.lastIndexOf("-"));
+////		System.out.println("appname="+appname);
+//		
+//	}
+
+}
